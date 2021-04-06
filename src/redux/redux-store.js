1660d0a73dc185218d1bb66bf186294c46f2f3a3
David-Bon <davsd333@gmail.com>
2021-04-07 01:21:11 +0300

    "localStorage added"

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,7 +5,12 @@ import thunk from "redux-thunk";
 const reducers = combineReducers({
     Reducer1
 })
+const persistedState =  localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {};
 
-let store = createStore(reducers, applyMiddleware(thunk));
+let store = createStore(reducers, persistedState, applyMiddleware(thunk));
+
+store.subscribe(()=>{
+    localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+});
 
 export default store
