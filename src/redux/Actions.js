e5d8625aa1c55a5640b7939215c788f49b67fc3b
David-Bon <davsd333@gmail.com>
2021-04-07 01:14:23 +0300

    "deleted unused actions"

--- src/redux/Actions.js
+++ src/redux/Actions.js
@@ -1,7 +1,6 @@
 import moment from 'moment'
 export const TOGGLE_PAUSE = "TOGGLE_PAUSE"
 export const NEW_TIMER = "NEW_TIMER"
-export const TIMER_TICK = "TIMER_TICK"
 export const TogglePauseAction = payload => ({
     type: TOGGLE_PAUSE,
     payload
@@ -13,19 +12,8 @@ const NewTimerAction = payload => ({
 
 })
 
-// const Tick = (timerId) => ({type: TIMER_TICK, timerId})
-
 export const CreateNewTimer = (payload) => (dispatch, getState) => {
     let newId = getState().Reducer1.timers.length+1
 let newTimer = {id: newId, name: payload, startedTime: moment().valueOf(), paused: false}
 dispatch(NewTimerAction(newTimer))
 }
-
-// const TickCreator = () => (dispatch, getState) => {
-//     let timersList = getState().Reducer1.timers
-//     let second = setInterval(function() {
-//         startTimestamp.add(1, 'second');
-//         return startTimestamp.format('HH:mm:ss');
-//     }, 1000);
-//
-// }
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,4 @@
-import {NEW_TIMER, TIMER_TICK, TOGGLE_PAUSE} from "./Actions";
+import {NEW_TIMER, TOGGLE_PAUSE} from "./Actions";
 
 let initialState = {
     timers: []
@@ -16,8 +16,6 @@ export const Reducer1 = (state = initialState, action) => {
             const newItem = {...selectedItem, paused: !selectedItem['paused']};
             const newArray = [...state.timers.slice(0, idx), newItem, ...state.timers.slice(idx + 1)]
             return {...state, timers: newArray}
-        case TIMER_TICK:
-
         default:
             return state
     }
